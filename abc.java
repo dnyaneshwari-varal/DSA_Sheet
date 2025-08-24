c724b11d2b7d4a6a0b617354b865aacc0d7df950
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-08-24 11:38:21 +0530

    initial commit

---
+++ abc.java
@@ -0,0 +1,8 @@
+public class abc{
+    public static void main(String []args){
+        System.out.println("hello");
+        for(int i=0;i<5;i++){
+            System.out.print(i +" ");
+        }System.out.println();
+    }
+}
