0cb74340ca1089ad2b52a4d5edce34c1d2e472de
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-08-24 16:02:39 +0530

    commit

---
+++ Basic/InputOutput.java
@@ -0,0 +1,33 @@
+/*
+ Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+
+Use:-
+
+for C++ : cout << variable_name;
+for Java : System.out.print();
+for Python : print()
+for Javascript : console.log()
+
+Examples:
+Input(user gives value): 7
+
+Output: 7
+ */
+
+
+package Basic;
+
+import java.util.Scanner;
+
+public class InputOutput {
+     public static void printNumber(int x) {
+        System.out.println(x);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        printNumber(x);
+    }
+}
