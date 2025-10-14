66dcaabb2386fb942fc05724658fb6e911890d39
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-10-14 19:07:44 +0530

    basic math

---
+++ basic_maths/Divisor.java
@@ -0,0 +1,34 @@
+package basic_maths;
+
+public class Divisor {
+    public static int[] divisor(int n){
+        int div[]=new int[n];
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                div[count++]=i;
+            }
+        }
+        
+        return div;
+    }
+    public static void main(String str[]){
+        int num=12;
+        int div[]=divisor(num);
+        for(int i=0;i<div.length;i++){
+            if(div[i]==0){
+                break;
+            }
+            System.out.println(div[i]);
+        }
+    }
+    
+}
+/*
+1
+2
+3
+4
+6
+12 
+*/
---
+++ basic_maths/amrstrong.java
@@ -0,0 +1,49 @@
+//An Amrstrong number is a number that is equal to the sum of its own digits each raised to the power of the number of digits.
+//Problem Statement: Given an integer N, return true it is an Armstrong number otherwise return false.
+package basic_maths;
+import java.util.*;
+public class amrstrong {
+    public static boolean isArmstrong(int num){
+        int temp=num;
+        int count=0;
+        while(temp>0){
+            count++;
+            temp=temp/10;
+        }
+        
+        int sum=0;
+        temp=num;
+        while(temp>0){
+            int rem=temp%10;
+            int mult=1;
+            for(int i=1;i<=count;i++){
+                mult=mult*rem;
+            }
+            temp=temp/10;
+            sum=sum+mult;
+        }
+        if(num==sum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String []str){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter num: ");
+        int num=sc.nextInt();
+        System.out.println(isArmstrong(num));
+        
+    }
+    
+}
+/*
+Enter num: 
+432
+false
+
+Enter num: 
+153
+true
+
+ */
