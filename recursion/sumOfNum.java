a5ba793279b10052c049c9cfd8b53a072236f302
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-10-20 22:11:07 +0530

    rrecursion

--- recursion/sumOfNum.java
+++ recursion/sumOfNum.java
@@ -1,7 +1,7 @@
 //Given a number ‘N’, find out the sum of the first N natural numbers.
 //ex=5=1+2+3+4+5=15
 package recursion;
-
+/* 
 public class sumOfNum {
     static void sum(int i,int n,int add){
 
@@ -21,10 +21,27 @@ public class sumOfNum {
         sum(1,n,0);
     }
 }
-/*
+
 We can use the formula for the sum of N numbers, i.e N(N+1)/2.
 For eg: N=5
 5(5+1)/2 = 5(6)/2 = 15.
 
- */
-/*The sum of the first 5 numbers is: 15 */
+
+The sum of the first 5 numbers is: 15 */
+public class sumOfNum{
+ public static void sum(int x,int n){
+    if(x==0){
+        System.out.println(n);
+        return;
+       
+    }
+    n=n+x;
+    sum(x-1,n);
+
+}
+public static void main(String[] str){
+    int x=5;
+    sum(x,0);
+}
+}
+
