5afb61757abf52c40a79f13049d7f2de2e698edd
dnyaneshwari_varal <varaldnyaneshwari@gmail.com>
2025-11-17 19:54:33 +0530

    dsa problem

--- recursion/factorial.java
+++ recursion/factorial.java
@@ -1,37 +1,21 @@
 package recursion;
-//iterative solution
-/*public class factorial {
-    public static int fact(int x){
-        int res=1;
-        for(int i=x;i>0;i--){
-            res=res*i;
-        }
-        return res;
 
-
-    }
-    public static void main(String[] arg){
-        int x=5;
-        int result=fact(x);
-        System.out.println("Factorial of "+x +" is: "+result);
-    }
-
-}
-    */
 
 public class factorial{
-    public static int fact(int n,int result){
-        if(n==0){
-            return result;
+    public static int fact(int num,int x){
+         if(num==1){
+            return 1;
+            
         }
-        result=result*n;
-        return fact(n-1,result);
+        x=num*x;
+        return fact(num-1, x);
+       
 
     }
     public static void main(String[] args){
-        int x=5;
-        int res=1;
-        int result=fact(x,res);
+        int num=5;
+        int x=1;
+        int result=fact(num,x);
         System.out.println("Factorial of " + x + " is: " + result);
 
     }
